cee7bea0a01e0fafc2c7e396ab3ee16b05c41ce1
luisahug <0356177@feevale.br>
2024-09-18 10:01:25 -0300

    Adicionado verificação de caixas vazias, ficando vermelho se estiver vazio

--- form-validacao.js
+++ form-validacao.js
@@ -9,3 +9,15 @@ function exibirDados(){
         '<br>CPF: ' + cpf + '<br>Telefone: ' + telefone +
         '<br>Endereço: ' + endereco + '<br>Email: ' + email)
 }
+
+$(document).ready(function(){
+    $("input").blur(function(){
+     if($(this).val() == "")
+         {
+             $(this).css({"border-color" : "#F00", "padding": "2px"});
+         }
+    else {
+        $(this).css({"border-color" : "#000"});
+         }
+    });
+})
