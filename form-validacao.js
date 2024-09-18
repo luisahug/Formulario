88ff4ecb898605e09b42cadf666d435bc69f6a68
luisahug <0356177@feevale.br>
2024-09-18 16:46:33 -0300

    Utiliza o JQuer Validator para validar os requisitos, além de exibir mensagens personalizadas para cada requisito não cumprido. Também exibe as informações do formulário abaixo dele após enviar ele (alterado chamada da funcao exibirDados para dentro do script de validação para que só seja exibido se tiver tudo certo)

--- form-validacao.js
+++ form-validacao.js
@@ -24,6 +24,38 @@ $(document).ready(function(){
         }
     });
 })
-
-$("#cadastro").validate();
+//validar se todos os campos "required" estão preenchidos e atendendo seus requisitos
+ $("#cadastro").validate({
+    submitHandler: function(form, event){
+        event.preventDefault();
+        exibirDados();
+    },
+    errorPlacement: function(error, element){
+        element.css({"border-color" : "#F00"});
+        error.insertAfter(element);
+    },
+    success: function(label, element){
+        $(element).css({"border-color": "#0F0"});
+    },
+    messages: {
+        nome: {
+            required: "Este campo é obrigatório"
+        },
+        cpf: {
+            required: "Este campo é obrigatório",
+            minlength: "Informe um CPF/CNPJ válido"
+        },
+        telefone: {
+            required: "Este campo é obrigatório",
+            minlength: "Informe um CPF/CNPJ válido"
+        },
+        endereco: {
+            required: "Este campo é obrigatório"
+        },
+        email: {
+            required: "Este campo é obrigatório",
+            email: "Informe um e-mail válido"
+        }
+    }
+ });
 
