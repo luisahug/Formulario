ece54734337babf32ec75452e38e447fd3c4d9d5
luisahug <0356177@feevale.br>
2024-09-18 11:10:39 -0300

    Adicionado script que valida se todos os campos estão preenchidos

--- form-validacao.js
+++ form-validacao.js
@@ -11,13 +11,64 @@ function exibirDados(){
 }
 
 $(document).ready(function(){
+    var cont = 0;
     $("input").blur(function(){
-     if($(this).val() == "")
+        if($(this).val() == "")
          {
              $(this).css({"border-color" : "#F00"});
          }
-    else if ($(this).val() != ""){
-        $(this).css({"border-color" : "#0F0"});
-         }
+        else if ($(this).val() != "" && $(this).val() !="Enviar")
+        {
+            $(this).css({"border-color" : "#0F0"});
+        }
     });
+    $("#btEnviar").click(function(event){
+        var cont = 0;
+        $("#cadastro input").each(function(){
+            if($(this).val() == ""){
+                cont++;
+                $(this).css({"border-color" : "#F00"});
+            }
+        });
+        if(cont==0){
+            exibirDados();
+        }
+        else {
+            event.preventDefault();
+            alert("Por favor, preencha todos os campos obrigatórios.");
+        };
+    });
+
+
 })
+ 
+/*
+$(document(function(){
+    var validator = $("#cadastro").validate(
+        rules: {
+            nome:{
+                required: true 
+            },
+            cpf:{
+                required: true,
+                number: true,
+                minlength:11,
+                maxlength:14
+            },
+            telefone:{
+                required: true,
+                number: true,
+                minlength: 11,
+                maxlength: 11
+            },
+            endereco:{
+                required: true
+            },
+            email:{
+                required: true,
+
+            }
+        }
+    )
+}))
+ */
