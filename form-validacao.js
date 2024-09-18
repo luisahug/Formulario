5f8afd546700ca1ae091abe0b92e365ae577e8f9
luisahug <0356177@feevale.br>
2024-09-18 09:40:49 -0300

    Adicionado scrip que exibe respostas do usuário abaixo do formulário, alterado nome do arquivo html

---
+++ form-validacao.js
@@ -0,0 +1,11 @@
+function exibirDados(){
+    var nome = $('#nome').val();
+    var cpf = $('#cpf').val();
+    var telefone = $('#telefone').val();
+    var endereco = $('#endereco').val();
+    var email = $('#email').val();
+
+    $('#dados').html('<p></p>Nome: ' + nome + 
+        '<br>CPF: ' + cpf + '<br>Telefone: ' + telefone +
+        '<br>Endereço: ' + endereco + '<br>Email: ' + email)
+}
